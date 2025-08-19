a1d358a98945dc085a66c0330b9e59704d03c072
filzausman <filzausman@28.gmail.com>
2025-08-19 17:26:45 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+function sum(a, b) {
+  return a + b;
+}
+
+console.log("Hello CI/CD!");
+module.exports = sum;
---
+++ test.js
@@ -0,0 +1,8 @@
+const sum = require("./app");
+if (sum(2, 3) === 5) {
+  console.log("✅ Test Passed");
+  process.exit(0);
+} else {
+  console.log("❌ Test Failed");
+  process.exit(1);
+}
